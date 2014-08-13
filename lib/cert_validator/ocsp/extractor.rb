c4fd49adefd1c93303c3de7de3685d27917dd2e2
Bryce Kerley <bkerley@brycekerley.net>
2014-08-13 18:46:52 -0400

    extract ocsp endpoint

--- lib/cert_validator/ocsp/extractor.rb
+++ lib/cert_validator/ocsp/extractor.rb
@@ -6,6 +6,30 @@ class CertValidator
       def initialize(cert)
         @certificate = cert
       end
+
+      def endpoint
+        return nil unless has_ocsp_extension?
+
+        ocsp_extension_payload
+      end
+
+      def has_ocsp_extension?
+        !! ocsp_extension
+      end
+
+      def ocsp_extension
+        @ocsp_extension ||= certificate.extensions.detect{ |e| e.oid == 'authorityInfoAccess' }
+      end
+
+      def decoded_extension
+        @decoded_extension ||= Asn1.new(Asn1.new(ocsp_extension).extension_payload).decode
+      end
+
+      def ocsp_extension_payload
+        content = decoded_extension.value.detect do |v|
+          v.first.value == 'OCSP'
+        end.value[1].value
+      end
     end
   end
 end
--- spec/ocsp_extractor_spec.rb
+++ spec/ocsp_extractor_spec.rb
@@ -8,13 +8,21 @@ describe CertValidator::RealOcspValidator::Extractor do
   describe 'with one OCSP endpoint' do
     subject { described_class.new cert 'good' }
 
+    it 'has an OCSP extension' do
+      expect(subject.has_ocsp_extension?).to be
+    end
+
     it 'extracts the OCSP endpoint' do
-      expect(subject.endpoint).to eq 'http://localhost:22022/crl'
+      expect(subject.endpoint).to eq 'http://localhost:22022/ocsp'
     end
   end
 
   describe 'with no OCSP endpoint' do
     subject { described_class.new cert 'crl_only' }
+
+    it 'has no OCSP extension' do
+      expect(subject.has_ocsp_extension?).to_not be
+    end
 
     it 'extracts no OCSP endpoint' do
       expect(subject.endpoint).to be_nil
