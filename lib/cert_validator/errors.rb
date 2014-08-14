f46178fa40b6713f756a5b0e7ee2e6c28c9d935c
Bryce Kerley <bkerley@brycekerley.net>
2014-08-14 18:15:14 -0400

    ocsp errors

--- lib/cert_validator/errors.rb
+++ lib/cert_validator/errors.rb
@@ -19,4 +19,63 @@ class CertValidator
       super "Couldn't fetch CRL."
     end
   end
+
+  module OcspFailures
+    class OcspFailure < Error
+    end
+
+    class FetchError < OcspFailure
+      def initialize
+        super "Couldn't fetch OCSP."
+      end
+    end
+
+    class NonzeroStatus < OcspFailure
+      def initialize(status)
+        super "OCSP status was #{status}, expected 0"
+      end
+    end
+
+    class ResponseMismatch < OcspFailure
+      def initialize
+        super "OCSP response did not match certificate issuer"
+      end
+    end
+
+    class MissingStatus < OcspFailure
+      def initialize
+        super "OCSP response was missing status section"
+      end
+    end
+
+    class UnacceptableNonce < OcspFailure
+      def initialize
+        super "OCSP response had unacceptable result from nonce check"
+      end
+    end
+
+    class SerialMismatch < OcspFailure
+      def initialize(got, expected)
+        super "OCSP response serial was #{got.inspect}, expected #{expected.inspect}"
+      end
+    end
+
+    class NotValidNow < OcspFailure
+      def initialize(validity_range)
+        super "OCSP response only valid from #{validity_range.begin} to #{validity_range.end}, currently #{Time.now}"
+      end
+    end
+
+    class Revoked < OcspFailure
+      def initialize
+        super "OCSP response indicates cert was revoked"
+      end
+    end
+
+    class UnexpectedStatus < OcspFailure
+      def initialize(got)
+        super "OCSP response was #{got.inspect}, expected 0"
+      end
+    end
+  end
 end
